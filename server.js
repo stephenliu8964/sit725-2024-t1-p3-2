21a51bb5ca476cb5830c1bb4b30c8642a5bb9540
stephenliu8964 <dq89_64@hotmail.com>
2024-03-12 16:08:04 +1100

    updated server js to have an api

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.get('/', (req, res) => {
     res.render('index.html');
 });
 
+app.get('/addTwoNumbers', (req, res) => {
+    let num1 = req.query.number1;
+    let num2 = req.query.number2;
+    let result = parseInt(num1) + parseInt(num2);
+
+    let response = {data:result, code: 200, message: 'successful'};
+    res.json(response);
+});
+
 app.listen(port, () => {
     console.log('server started');
 });
